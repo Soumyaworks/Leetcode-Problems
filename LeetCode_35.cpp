509406d71996b00b50025d02b20c4a5e06d03d0f
SOUMYA BANERJEE <54377824+Soumyaworks@users.noreply.github.com>
2022-02-26 14:03:50 +0530

    Create LeetCode_35.cpp

---
+++ LeetCode_35.cpp
@@ -0,0 +1,13 @@
+ int searchInsert(vector<int>& nums, int target) {
+        int low=0,high=nums.size()-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(target<nums[mid])
+                high=mid-1;
+            else if(target>nums[mid])
+                low=mid+1;
+            else if(target==nums[mid])
+                return mid;
+        }
+        return low;
+    }
